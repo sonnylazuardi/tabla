622502eef124127854b54e6f58f9b4d46c2860ab
sonny.lazuardi <sonny.lazuardi@grabtaxi.com>
2022-03-25 17:49:02 +0800

    Add basic design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,67 @@
+import { format } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import useDarkMode from "use-dark-mode";
 
 const Home: NextPage = () => {
+  const darkMode = useDarkMode();
+  React.useEffect(() => {
+    document.body.addEventListener("mousewheel", function (event: any) {
+      event.preventDefault();
+      document.body.scrollLeft += event.deltaY;
+    }, { passive: false });
+  }, [])
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="app" id="scroll_container">
       <Head>
-        <title>Create Next App</title>
+        <title>Tabla</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      
+      <div className="absolute top-0 left-0 right-0 pt-16">
+        <h2 className='order-first font-semibold text-lg tracking-wide text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text'>Tabla</h2>
+          <h1 className='text-6xl font-extrabold uppercase text-transparent tracking-tighest sm:text-5xl lg:text-7xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text'>{format(new Date(), "hh.mm aaaaa'm'")}</h1>
+          <div className='text-2xl'>{format(new Date(), "LLLL dd", {locale: enUS})}</div>
+          {/* <button onClick={() => darkMode.toggle()}>Click me</button> */}
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      <div className="wrap space-x-4 p-4 pb-16">
+          {/* <div className="tab">
+            <iframe style={frameStyle} src="" />
+          </div>
+          <div className="tab">
+            <iframe style={frameStyle} src="" />
+          </div>
+          <div className="tab">
+            <iframe style={frameStyle} src="" />
+          </div> */}
+          {/* <div className="tab">
+            <iframe style={frameStyle} src="https://kotla.vercel.app" />
+          </div> */}
+          {/* <div className="tab">
+            <iframe style={frameStyle} src="https://nerdlegame.com/" />
+          </div> */}
+          <div className="tab">
+            <iframe
+              is="x-frame-bypass"
+              style={frameStyle}
+              src="https://nytimes.com/games/wordle/index.html"
+            />
+          </div>
+          <div className="tab">
+            <iframe style={frameStyle} src="https://katla.vercel.app" />
+          </div>
+        </div>
+        
     </div>
   )
 }
+
+const frameStyle = {
+  border: "none",
+  width: "100%",
+  height: "100%"
+};
 
 export default Home
